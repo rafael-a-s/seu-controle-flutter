fba63c8736633949748af07f378dea8631fb6400
Rafael Aguiar <rafaelaguiar@unitins.br]>
2023-08-03 19:53:16 -0300

    feat: Cria rota para home e garante instancia ctrl

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,9 +1,15 @@
+import 'package:clean_architeture_flutter/app_widget.controller.dart';
+import 'package:clean_architeture_flutter/features/presenter/modules/home/home.module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
   @override
-  final List<Bind> binds = [];
+  final List<Bind> binds = [
+    Bind.factory((i) => AppWidgetController()),
+  ];
 
   @override
-  List<ModularRoute> get routes => [];
+  List<ModularRoute> get routes => [
+        ModuleRoute('/home', module: HomeModule()),
+      ];
 }
