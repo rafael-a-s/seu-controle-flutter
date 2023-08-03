b338295ccf67a12af99d78922ac9098e4216d66f
Rafael Aguiar <rafaelaguiar@unitins.br]>
2023-08-03 19:53:58 -0300

    feat: Novo controller e adiciona Riverpod

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,17 +1,28 @@
+import 'package:clean_architeture_flutter/app_widget.controller.dart';
 import 'package:clean_architeture_flutter/features/presenter/base/base_widget.dart';
 import 'package:clean_architeture_flutter/features/presenter/theme/color_schemes.g.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulHookConsumerWidget {
   const AppWidget({super.key});
 
   @override
+  AppWidgetState createState() => AppWidgetState();
+}
+
+class AppWidgetState extends ConsumerState<AppWidget> {
+  @override
   Widget build(BuildContext context) {
+    final themeMode = ref
+        .watch(appWidgetControllerProvider.select((value) => value.themeMode));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
       darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      themeMode: themeMode,
       builder: (context, child) => BaseWidget(child: child ?? Container()),
       initialRoute: "/home",
     ).modular();
