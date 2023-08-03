9cf6132bef61f0fa7f7d80a97fc13c308a9afe76
Rafael Aguiar <rafaelaguiar@unitins.br]>
2023-08-03 19:57:43 -0300

    feat: Nova app bar, troca de tema claro e escuro

--- lib/features/presenter/base/base_widget.dart
+++ lib/features/presenter/base/base_widget.dart
@@ -1,13 +1,29 @@
+import 'package:clean_architeture_flutter/app_widget.controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class BaseWidget extends StatelessWidget {
+class BaseWidget extends HookConsumerWidget {
   final Widget child;
 
   const BaseWidget({super.key, required this.child});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Arquitetura Rafael'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              SchedulerBinding.instance.addPostFrameCallback((_) {
+                ref.read(appWidgetControllerProvider.notifier).changeTheme();
+              });
+            },
+            icon: const Icon(Icons.nightlight),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           Expanded(
