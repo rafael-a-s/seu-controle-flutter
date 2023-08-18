c06d8ca4d3aae686c5fddd1a90624cf5b0fa9950
Rafael Aguiar <rafaelaguiar@unitins.br]>
2023-08-17 23:26:07 -0300

    fix: Altera cor primária

--- lib/features/presenter/theme/color_schemes.g.dart
+++ lib/features/presenter/theme/color_schemes.g.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const lightColorScheme = ColorScheme(
   brightness: Brightness.light,
-  primary: Color(0xFF6750A4),
+  primary: Color(0xFF6443BD),
   onPrimary: Color(0xFFFFFFFF),
   primaryContainer: Color(0xFFEADDFF),
   onPrimaryContainer: Color(0xFF21005D),
