e9fb86c867dad3b1b52011d73d7d382e249227f9
rafael-a-s <rafaelaguiar@unitins.br>
2023-12-04 16:41:12 -0300

    fix: Remove import de list de expemplo

--- lib/features/presenter/modules/exemplo/exemplo.module.dart
+++ lib/features/presenter/modules/exemplo/exemplo.module.dart
@@ -1,4 +1,3 @@
-import 'package:clean_architeture_flutter/features/presenter/modules/exemplo/pages/list/list.page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class ExemploModule extends Module {
@@ -10,10 +9,6 @@ class ExemploModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute(
-          "/",
-          child: (_, __) => const ListExemplo(),
-          transition: TransitionType.rightToLeft,
-        ),
+
       ];
 }
