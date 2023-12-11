bf373da413c544c957773fa110b52d2ee05a7ba4
rafael-a-s <rafaelaguiar@unitins.br>
2023-12-11 15:41:49 -0300

    feat: create use case de criar user

---
+++ lib/features/domain/usecases/user/create_user.usecase.dart
@@ -0,0 +1,18 @@
+import 'package:clean_architeture_flutter/core/erros/failures.dart';
+import 'package:clean_architeture_flutter/core/usecase/usecase.dart';
+import 'package:clean_architeture_flutter/features/domain/entity/user.entity.dart';
+import 'package:clean_architeture_flutter/features/domain/repositories/user_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class CreateUserUsecase implements Usecase<User, User> {
+  late final UserRepository repositorie;
+
+  CreateUserUsecase(this.repositorie);
+
+  @override
+  Future<Either<Failure, User>> call(User user) async {
+    return user.props.isNotEmpty
+        ? await repositorie.create(user)
+        : Left(NullParamFailure());
+  }
+}
