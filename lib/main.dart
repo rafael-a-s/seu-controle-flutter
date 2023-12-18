956e34e7f67535c90331f895ac176f6795e36fbc
rafael-a-s <rafaelaguiar@unitins.br>
2023-12-18 17:31:20 -0300

    feat: Inicia o hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'dart:io';
+
 import 'package:clean_architeture_flutter/app_module.dart';
 import 'package:clean_architeture_flutter/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive/hive.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
+
+  var path = Directory.current.path;
+  Hive.init(path);
 
   runApp(
     const ProviderScope(
