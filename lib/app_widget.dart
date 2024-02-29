d0371e5c6fe9455f33c868e1cea255d963a9b19a
rafael-a-s <rafaelaguiar@unitins.br>
2024-02-29 15:23:22 -0300

    feat: push pull down implementado

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
 class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
@@ -16,26 +17,46 @@ class AppWidget extends StatefulWidget {
 class AppWidgetState extends State<AppWidget> {
   @override
   Widget build(BuildContext context) {
-    return GlobalLoaderOverlay(
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.defaultTheme,
-        darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-        builder: (context, child) => LoaderOverlay(
-          useDefaultLoading: false,
-          overlayWidgetBuilder: (_) {
-            //ignored progress for the moment
-            return const Center(
-              child: SpinKitCubeGrid(
-                color: AppColors.primary,
-                size: 50.0,
-              ),
-            );
-          },
-          overlayColor: AppColors.white.withOpacity(.8),
-          child: child as Widget,
+    return RefreshConfiguration(
+      footerTriggerDistance: 15,
+      dragSpeedRatio: 0.91,
+      headerBuilder: () => MaterialClassicHeader(),
+      footerBuilder: () => ClassicFooter(),
+      enableLoadingWhenNoData: false,
+      enableRefreshVibrate: false,
+      enableLoadMoreVibrate: false,
+      shouldFooterFollowWhenNotFull: (state) {
+        // If you want load more with noMoreData state ,may be you should return false
+        return false;
+      },
+      child: GlobalLoaderOverlay(
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.defaultTheme,
+          darkTheme:
+              ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+          localizationsDelegates: const [
+            RefreshLocalizations.delegate,
+          ],
+          builder: (context, child) => ScrollConfiguration(
+            behavior: const ScrollBehavior(),
+            child: LoaderOverlay(
+              useDefaultLoading: false,
+              overlayWidgetBuilder: (_) {
+                //ignored progress for the moment
+                return const Center(
+                  child: SpinKitCubeGrid(
+                    color: AppColors.primary,
+                    size: 50.0,
+                  ),
+                );
+              },
+              overlayColor: AppColors.white.withOpacity(.8),
+              child: child as Widget,
+            ),
+          ),
+          routerConfig: Modular.routerConfig,
         ),
-        routerConfig: Modular.routerConfig,
       ),
     );
   }
--- lib/features/presenter/modules/home/pages/home_page.dart
+++ lib/features/presenter/modules/home/pages/home_page.dart
@@ -13,6 +13,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:loader_overlay/loader_overlay.dart';
+import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
 class HomePage extends StatefulHookConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -29,6 +30,20 @@ class _HomePage extends ConsumerState<HomePage> {
       ref.read(homeStateProvider.notifier).loadingPage();
     });
   }
+
+  _fetchData() async {
+    ref.read(homeStateProvider.notifier).loadingPage();
+
+    _refreshController.loadComplete();
+  }
+
+  _refreshData() async {
+    ref.read(homeStateProvider.notifier).loadingPage();
+    setState(() {});
+    _refreshController.refreshCompleted();
+  }
+
+  final _refreshController = RefreshController(initialRefresh: false);
 
   @override
   Widget build(BuildContext context) {
@@ -48,113 +63,124 @@ class _HomePage extends ConsumerState<HomePage> {
       body: Padding(
         padding: AppDefaults.padinngDefault,
         child: SafeArea(
-          child: CustomScrollView(
-            slivers: [
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 10,
+          child: SmartRefresher(
+            enablePullDown: true,
+            enablePullUp: true,
+            header: const WaterDropHeader(),
+            controller: _refreshController,
+            onLoading: _fetchData,
+            onRefresh: _refreshData,
+            child: CustomScrollView(
+              slivers: [
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 10,
+                  ),
                 ),
-              ),
-              AppBarHomeComponent(
-                id: authUser!.id,
-                name: authUser.name,
-              ),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 30,
+                AppBarHomeComponent(
+                  id: authUser!.id,
+                  name: authUser.name,
                 ),
-              ),
-              SliverToBoxAdapter(
-                child: WageInfoComponent(
-                  wage: financeControlMetrics!.totalRemuneration ?? 0.0,
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 30,
+                  ),
                 ),
-              ),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 30,
+                SliverToBoxAdapter(
+                  child: WageInfoComponent(
+                    wage: financeControlMetrics!.totalRemuneration ?? 0.0,
+                  ),
                 ),
-              ),
-              SliverToBoxAdapter(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap: () => Modular.to.pushNamed(AppRoutes.typeExpense),
-                      child: CardExpenseComponent(
-                        totalExpense:
-                            financeControlMetrics.totalExpenses ?? 0.0,
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 30,
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () =>
+                            Modular.to.pushNamed(AppRoutes.typeExpense),
+                        child: CardExpenseComponent(
+                          totalExpense:
+                              financeControlMetrics.totalExpenses ?? 0.0,
+                        ),
                       ),
-                    ),
-                    Column(
-                      children: [
-                        CardTotalSpendWeekComponent(
-                            totalSpendForWeek:
-                                financeControlMetrics.totalSpendForWeek ?? 0.0),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        GestureDetector(
-                          onTap: () => Modular.to
-                              .pushNamed(AppRoutes.monthlyContribution),
-                          child: CardMonthlyContributionComponent(
-                              totalContribution:
-                                  financeControlMetrics.totalInvestimentMonth ??
+                      Column(
+                        children: [
+                          CardTotalSpendWeekComponent(
+                              totalSpendForWeek:
+                                  financeControlMetrics.totalSpendForWeek ??
                                       0.0),
-                        ),
-                      ],
-                    )
-                  ],
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          GestureDetector(
+                            onTap: () => Modular.to
+                                .pushNamed(AppRoutes.monthlyContribution),
+                            child: CardMonthlyContributionComponent(
+                                totalContribution: financeControlMetrics
+                                        .totalInvestimentMonth ??
+                                    0.0),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 30,
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 30,
+                  ),
                 ),
-              ),
-              SliverToBoxAdapter(
-                child: CardSubTotalMonthlyComponent(
-                    subTotal: financeControlMetrics.subTotalForMonth ?? 0.0),
-              ),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 30,
+                SliverToBoxAdapter(
+                  child: CardSubTotalMonthlyComponent(
+                      subTotal: financeControlMetrics.subTotalForMonth ?? 0.0),
                 ),
-              ),
-              SliverToBoxAdapter(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Fontes de renda",
-                      style: AppDefaults.textStyleHeader2,
-                    ),
-                    GestureDetector(
-                      onTap: () => Modular.to.pushNamed(AppRoutes.remuneration),
-                      child: Text(
-                        "Gerenciar",
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 30,
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Fontes de renda",
                         style: AppDefaults.textStyleHeader2,
                       ),
-                    ),
-                  ],
+                      GestureDetector(
+                        onTap: () =>
+                            Modular.to.pushNamed(AppRoutes.remuneration),
+                        child: Text(
+                          "Gerenciar",
+                          style: AppDefaults.textStyleHeader2,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 30,
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 30,
+                  ),
                 ),
-              ),
-              const SliverToBoxAdapter(
-                child: CardTransactionComponent(),
-              ),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 20,
+                const SliverToBoxAdapter(
+                  child: CardTransactionComponent(),
                 ),
-              ),
-              const SliverToBoxAdapter(
-                child: CardTransactionComponent(),
-              ),
-            ],
+                const SliverToBoxAdapter(
+                  child: SizedBox(
+                    height: 20,
+                  ),
+                ),
+                const SliverToBoxAdapter(
+                  child: CardTransactionComponent(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
